ed4b7e03ee764f87e523ed994c04d5d88dafbd04
Aashiish-Pandey <ash006pandey@gmail.com>
2021-10-28 00:33:56 +0530

    testing classbased component(headline)

--- src/component/header/spect.test.js
+++ src/component/header/spect.test.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Header from "./index";
+import {findByTestAtrr} from '../../Utils'
 
 const setUp = (props = {}) => {
   const component = shallow(<Header {...props} />);
   return component;
 };
+
+
 
 describe("Header component", () => {
   let component;
@@ -15,13 +18,14 @@ describe("Header component", () => {
 
   it("it should render without errors", () => {
     console.log(component.debug());
-
-    const wrapper = component.find(`[ data-test="headerComponent"]`);
+   const wrapper= findByTestAtrr(component,"headerComponent")
+    
     expect(wrapper.length).toBe(1);
   });
 
   it("it should render a logo", () => {
-    const logo = component.find(`[data-test='logoIMG']`);
-    expect(logo.length).toBe(1);
+    const wrapper= findByTestAtrr(component,"logoIMG")
+    
+    expect(wrapper.length).toBe(1);
   });
 });
